bbe2d18e5143a1fcb4fc20c3df38a7693c43fd36
NITESHDHIMAL <niteshdhimal18@gmail.com>
2024-12-08 15:28:52 +0545

    hme and contact page create

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <>
+
+      <Routes>
+        <Route path='' element={<Home/>} />
+        <Route path='/contact' element={<Contact/>} />
+      </Routes>
+
+    </>
+  )
+}
+
+export default App
